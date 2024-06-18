929a23c535390c8fa686c6ec68ad3aa51d0a649f
Alex LaPierre <aslapierre7@gmail.com>
2024-06-18 17:58:17 -0500

    populated readme, added walkthrough video

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -37,14 +37,31 @@ router.post('/', (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
-  Tag.update({
-    where: {
-      id: req.params.id
+// router.put('/:id', (req, res) => {
+//   Tag.update({
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//   res.json({ message: 'Tag updated!'})
+//   // update a tag's name by its `id` value
+// });
+
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id, // Include the `where` attribute
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
     }
-  })
-  res.json({ message: 'Tag updated!'})
-  // update a tag's name by its `id` value
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End
+
+## Description
+The back-end E-commerce application allows you to have an online store and be able to update inventory with CRUD operations.
+
+## Purpose
+The purpose of the E-commerce application is to keep your inventory up to date.
+
+## Installation
+Visit GitHub and clone the repository titled E-Commerce-ASL to your local machine. Once cloned, go to server.js and run "npm i" or "npm install" to install the necessary dependencies.
+
+## Usage
+Once you've cloned the repository, you can use Insomnia to keep your inventory up to date!
+
+Attached is a walkthrough video of how to use the application through Insomnia.
+
+[CRUD Operations in Insomnia!](https://app.screencastify.com/v3/watch/ND3hCWyp89GvQrmJu7X4)
